eeb9a0cb17082dea0e1f154245ec79a9ffd1282c
Indradip <chadsigmarizzler@gmail.com>
2025-04-12 21:02:08 +0600

    first commit

---
+++ stringuppercase.py
@@ -0,0 +1,18 @@
+class IOString:
+    
+    #CONSTRUCTOR
+    def __init__(self):
+        self.str1 = ""
+    
+    #METHOD/FUNCTION
+    def getString(self):
+        self.str1 = input("Enter a String: ")
+    
+    #METHOD/FUNCTON
+    def printString(self):
+        print(f"Result is: {self.str1.upper()}")
+    
+object1 = IOString()
+
+object1.getString()
+object1.printString()
